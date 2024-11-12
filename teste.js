7bcb580d5e6f26633221885d5034472ac1bc0892
Eduarda Dornelles Behling <182400588@senacinformatica.br>
2024-11-12 16:17:34 -0300

    aplicação da duda

---
+++ teste.js
@@ -0,0 +1,39 @@
+
+async function getData() {
+    const url = "https://pokeapi.co/api/v2/pokemon";
+    try {
+        const response = await fetch (url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`); 
+    }
+        const json = await response.json();
+        console.log(json);
+
+        const listContainer = document.getElementById(`lista`);
+
+        json.results.forEach(async (pokemon) => {
+
+            const currentPokemon = await fetch(pokemon.url)
+            const currentPokemonJson = await currentPokemon.json()
+
+            console.log(currentPokemonJson)
+
+            const element = `
+            <li>
+            <h2>Nº${currentPokemonJson.id}</h2>
+             <img src="${currentPokemonJson.sprites.other.home.front_default}" alt="foto do pokemon" />
+             <p>${currentPokemonJson.name.toUpperCase()}</p>
+            </li>
+            `
+
+            listContainer.innerHTML += element
+
+            });
+        } catch (error) {
+        console.error(error.message);
+
+        }
+    }
+
+    getData()
+        
